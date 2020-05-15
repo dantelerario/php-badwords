5e21844cb892ad3ba3babd4f3048d100fc9cdac3
Dante L'Erario <dantelerario@gmail.com>
2020-05-15 14:40:46 +0200

    PHP done

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
     <title>php-badwords</title>
   </head>
   <body>
+    <?php
+      //Creare una variabile con un paragrafo di testo.
+      // Visualizzare a schermo il paragrafo
+      // Visualizzare a schermo la relativa lunghezza del paragrafo
+      // Sostituire tutte le ricorrenze, nel paragrafo creato, della badword passata in GET (query string) con tre * (asterschi).
+
+      $text = 'Creare una variabile con un paragrafo di testo';
+
+      $replace = str_replace($_GET["name"], '***', $text);
+     ?>
+
+     <p><?php echo $text ?></p>
+
+     <p>Il paragrafo è lungo <?php echo strlen($text); ?></p>
+
+     <p><strong>Sostituzione della badword presa con GET con ***: </strong> <?php echo $replace; ?></p>
+
+
+
 
   </body>
 </html>
